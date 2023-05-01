feaa917cc665d8e59d7b87bc2997fa95a60da96f
Abbey <84262713+abhi5157@users.noreply.github.com>
2023-05-01 20:10:29 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -26,6 +26,13 @@ FOR h IN hotels
     "Total chain" : length,
   }
 
+
+ //Ranking Hotels in particular city 
+  FOR h IN Hotels
+ // RETURN u.friends[* RETURN CONCAT(CURRENT.name, " is a friend of ", u.name)]
+ filter lower(h.city) == "new delhi" && h.hotel_star_rating != null
+    RETURN CONCAT(h.property_name, "is a hotel found in  ", h.city, "and the Rating of the hotels are ",h.hotel_star_rating )
+    
 --   //graph representation
 for h in hotels
 FILTER h.country == "India"
@@ -42,3 +49,113 @@ FOR h IN hotels
       RETURN h
   ) 
   RETURN { recommendations : recommendations }
+  
+  
+  //returning those hotels which provide free private parking
+  FOR doc IN perfectView
+   SEARCH PHRASE(doc.hotel_description, 'Free private parking','text_en'
+  ) 
+  RETURN 
+  GEO_POINT(doc.longitude,doc.latitude)
+  
+  
+  //those places whose have parking facilities and available in mumbai with high rating and 24 hrs available
+
+  FOR doc IN perfectView
+   SEARCH ANALYZER(doc.hotel_description == TOKENS("parking",  "text_en")[0] and
+   doc.hotel_description == TOKENS("24",  "text_en")[0]
+   And doc.city == "mumbai" and doc.hotel_star_rating !=null  , "text_en")
+  RETURN {
+   name : doc.Place,
+   rating : doc.hotel_star_rating,
+   city : doc.city
+   }
+   
+   
+   
+   
+   
+   // Filterout those Hotels which have Mountain view 
+   FOR doc IN perfectView
+   SEARCH ANALYZER( MIN_MATCH(
+   phrase(doc.hotel_facilities, 'Mountain view') ,
+   phrase(doc.hotel_facilities, 'room service') ,
+   phrase(doc.hotel_facilities, '
+   climb hill'),2 
+   ) ,'text_en'
+  ) 
+  RETURN {
+   name : doc.Place,
+   rating : doc.hotel_star_rating,
+   city : doc.city
+   }
+   
+   
+   //Filter Outh those cities which 
+   FOR doc IN perfectView
+
+   SEARCH ANALYZER( starts_with(doc.hotel_facilities, '24'), 'text_en' ) 
+   limit 30
+  RETURN {
+   name : doc.Place,
+   rating : doc.hotel_star_rating,
+   discription : doc.hotel_facilities,
+   city : doc.city
+   }
+   
+   //returning railway datas
+   for r in Railway
+   filter r.geometry != null
+   return r
+  
+  //those places whose rating are higher and came under cities
+FOR doc IN perfectView
+  SEARCH ANALYZER(doc.Distance == "4" AND doc.Ratings >3 , "text_en")
+  RETURN {
+   name : doc.Place,
+   rating : doc.Ratings,
+   kilometerFromCity : doc.Distance
+   }
+   
+   
+   //finding those travelling place with nearby station
+for r in Railway
+ filter r.properties.state =="Rajasthan"
+  for p in placesNkey
+  filter p.City == "Jaipur" and p.Ratings != null
+  
+   return {
+   "station" : r.properties.name,
+   "City" : p.City,
+   "Place" : p.Place,
+   "Distance" : p.Distance
+   }
+   
+   
+   
+   //Graph queries
+   //check  b/w cities and unique places
+for v,e,p IN 1..2 ANY
+'placesNkey/VenkatappaArtGallery' places2hotels
+options {uniqueVertices : 'path'}
+LIMIT 1000
+Return p
+
+
+//checking how many hotels present in particular city
+
+FOR h IN hotels3
+  COLLECT city = h.city WITH COUNT INTO length
+  RETURN { 
+    "city" : city, 
+    "count" : length 
+  }
+  
+  
+ //Ranking Hotels in particular city 
+  FOR h IN Hotels
+ // RETURN u.friends[* RETURN CONCAT(CURRENT.name, " is a friend of ", u.name)]
+ filter lower(h.city) == "new delhi" && h.hotel_star_rating != null
+    RETURN CONCAT(h.property_name, "is a hotel found in  ", h.city, "and the Rating of the hotels are ",h.hotel_star_rating )
+
+  
